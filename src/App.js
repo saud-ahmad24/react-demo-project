ef544c9877d4af0812208c7b85a3947336d231da
Garvitkumar Choraria <garvit@engees.in>
2022-11-23 17:07:42 +0530

    theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import NavBar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
+import React,{useState} from 'react'
 
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const toggleMode = ()=>{
+    if(mode==="dark"){
+      setMode('light')
+      document.body.style.backgroundColor='white';
+    }else{
+      setMode('dark')
+      document.body.style.backgroundColor='grey';
+    }
+  }
   return (
     <>
-    <NavBar  title="TextUtils"/>
-    <div className="container">
-    {/* <TextForm heading="Enter the text to analyze"/> */}
-    <About/>
+    <NavBar  title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <div className="container" >
+    <TextForm heading="Enter the text to analyze"  mode={mode}/>
+    {/* <About/> */}
 
     </div>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import propTypes from 'prop-types'
 export default function NavBar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+      <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           {props.title}
@@ -42,9 +42,15 @@ export default function NavBar(props) {
               Search
             </button>
           </form> */}
+          <div className={`form-check form-switch text-${props.mode==="light"?"dark":"light"}
+          `}>
+          <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
+</div>
         </div>
       </div>
     </nav>
+
   );
 }
 
